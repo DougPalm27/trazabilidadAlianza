c8c9e2efc26e2f5ab855c2dc9db09ef7a53167b1
DougPalm27 <dpalma@honducafeproyectos.com>
2024-02-24 10:26:45 -0600

    Primer commit

---
+++ includes/links.php
@@ -0,0 +1,12 @@
+<link rel="shortcut icon" sizes="196x196" href="../assets/images/logo.png">
+<!-- style -->
+<link rel="stylesheet" href="./assets/animate.css/animate.min.css" type="text/css" />
+<link rel="stylesheet" href="./assets/glyphicons/glyphicons.css" type="text/css" />
+<link rel="stylesheet" href="./assets/font-awesome/css/font-awesome.min.css" type="text/css" />
+<link rel="stylesheet" href="./assets/material-design-icons/material-design-icons.css" type="text/css" />
+
+<link rel="stylesheet" href="./assets/bootstrap/dist/css/bootstrap.min.css" type="text/css" />
+<!-- build:css ../assets/styles/app.min.css -->
+<link rel="stylesheet" href="./assets/styles/app.css" type="text/css" />
+<!-- endbuild -->
+<link rel="stylesheet" href="./assets/styles/font.css" type="text/css" />
